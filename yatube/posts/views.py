ee6646a7c24889d9254003e4c8679f587c2a82d8
Константин Николаев <crazylixx@yandex.ru>
2022-04-20 22:26:45 +0300

    review2 passed clear

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
+
+
+ON_PAGE = 10
 
 
 def index(request):
-    return HttpResponse('Будущее главной страницы')
+    template = 'posts/index.html'
+    posts = Post.objects.all()[:ON_PAGE]
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Группа {slug}, в которых пользователи пишут посты')
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:ON_PAGE]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
